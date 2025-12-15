35eaf1e723ff2da1532a821efe3e74d5bac1fd66
Amr Omran <amr.raafat.omran@gmail.com>
2025-12-15 09:30:49 +0100

    Initial Commit

---
+++ HIIT/HIITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITApp.swift
+//  HIIT
+//
+//  Created by Amr Omran on 15.12.25.
+//
+
+import SwiftUI
+
+@main
+struct HIITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
